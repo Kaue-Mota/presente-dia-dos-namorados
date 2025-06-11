5e5624971205fc45c79054a0ff351cbc949654e0
Kaue-Mota <pckaue123@gmail.com>
2025-06-11 13:17:56 -0300

    Add new pages with styled content and Spotify embeds
    - Created Page 3 with a heartfelt message and a Spotify track embed.
    - Created Page 4 reflecting on personal growth and love, including a Spotify track.
    - Created Page 5 expressing deep affection and support, featuring another Spotify track.
    - Added CSS styles for all pages to enhance visual appeal and maintain consistency.

--- src/App.jsx
+++ src/App.jsx
@@ -11,44 +11,52 @@ function App() {
   return (
     <>
     
-    <HeartBackground />
+   
     <section className="app-container">
       
       <Cards
-        imageUrl={'/images/dia-1-image.png'}
-        title={'DIA 1'}
-        description={'O início de uma surpresa especial: 14 textos em 14 dias, um gesto feito com amor, código e muita saudade. Um presente simples, mas cheio de sentimento.'}
-        color={'#FF5733'}
+        imageUrl={'/images/foto1.jpg'}
+        title={'CARTA 1'}
+        description={'O início de uma surpresa especial: alguns textos escritos com amor, código e muita saudade. Um presente simples, mas cheio de sentimento.'}
+        color={'rgba(112, 37, 37, 0.5)'}
         link={'./src/Pages/Page-1/index.html'}
         
       />
 
       <Cards
-        imageUrl={'/images/dia-1-image.png'}
-        title={'DIA 2'}
+        imageUrl={'/images/foto3.jpg'}
+        title={'CARTA 2'}
         description={'Escrito durante uma call, o texto transborda paixão, desejo contido e gratidão. Um momento de conexão sincera e abertura entre dois corações apaixonados.'}
+        color={'rgba(94, 10, 10, 0.5)'}
+        link={'./src/Pages/Page-2/index.html'}
        
       />
 
       <Cards
-        imageUrl={'/images/dia-1-image.png'}
-        title={'DIA 3'}
-        description={'Entre saudades e planos, nasce o desejo de uma vida a dois cheia de amor, estabilidade e sonhos realizados. Um futuro de rainha para ela, e de pai dedicado para ele.'}
+        imageUrl={'/images/foto4.jpg'}
+        title={'CARTA 3'}
+        description={' Um desabafo leve e apaixonado sobre como o amor dela cura qualquer dia ruim. Entre risadas e emoção, o desejo de casar e viver ao lado de quem transforma tudo.'}
+        color={'rgba(189, 19, 19, 0.5)'}
+        link={'./src/Pages/Page-3/index.html'}
       />
 
       <Cards
-        imageUrl={'/images/dia-1-image.png'}
-        title={'DIA 4'}
-        description={''}
+        imageUrl={'/images/foto5.jpg'}
+        title={'CARTA 4'}
+        description={'Uma reflexão sobre o passado, inseguranças superadas e a transformação trazida por um amor verdadeiro. Um agradecimento sincero à mulher que mudou tudo.'}
+        color={'rgba(121, 15, 33, 0.5)'}
+        link={'./src/Pages/Page-4/index.html'}
       />
 
       <Cards
-        imageUrl={'/images/dia-1-image.png'}
-        title={'DIA 5'}
-        description={'O início de uma surpresa especial: 14 textos em 14 dias, um gesto feito com amor, código e muita saudade. Um presente simples, mas cheio de sentimento.'}
+        imageUrl={'/images/foto6.jpg'}
+        title={'CARTA 5'}
+        description={'Uma carta de amor leve e sincera, sobre como ela se tornou o porto seguro em meio ao caos. Um lembrete de que, com ela, tudo faz sentido.'}
+        color={'rgba(119, 13, 13, 0.5)'}
+        link={'./src/Pages/Page-5/index.html'}
       />
 
-      
+       <HeartBackground />
     </section>
      
    
